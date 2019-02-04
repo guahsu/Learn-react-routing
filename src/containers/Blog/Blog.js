c866ff968b4c656bb4218d97ac43d24b30870ceb
GuaHsu <guaswork@gmail.com>
2019-02-04 10:54:29 +0800

    Preparing the project for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,82 +1,10 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-
-import Post from '../../components/Post/Post'
-import FullPost from '../../components/FullPost/FullPost'
-import NewPost from '../../components/NewPost/NewPost'
 import './Blog.css'
 
+import Posts from './Posts/Posts'
+
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    selectedPostContent: null,
-    selectedPostLoading: false,
-    error: false
-  }
-
-  componentDidMount () {
-    const random = Math.random().toFixed(1)
-    const url = random >= 0.3 ? '/posts' : 'WrongUrl'
-    console.log(`if random < 0.3, get wrong url, random: ${random}, url: ${url}`)
-    axios
-      .get(url)
-      .then(res => {
-        const posts = res.data.slice(0, 4)
-        const updatedPosts = posts.map(post => {
-          return {
-            ...post,
-            author: 'Max'
-          }
-        })
-        this.setState({ posts: updatedPosts })
-      })
-      .catch(err => {
-        console.log(err)
-        this.setState({ error: true })
-      })
-  }
-
-  postSelectedHandler = id => {
-    if (this.state.selectedPostId !== id) {
-      this.setState({ selectedPostLoading: true })
-      axios.get(`/posts/${id}`).then(res => {
-        this.setState({
-          selectedPostId: id,
-          selectedPostContent: res.data,
-          selectedPostLoading: false
-        })
-      })
-    }
-  }
-
-  postDeletePostHandler = () => {
-    axios.delete(`/posts/${this.state.selectedPostId}`).then(res => {
-      if (res.status === 200) {
-        window.alert('Delete Post Success !')
-        this.setState({
-          selectedPostId: null,
-          selectedPostContent: null
-        })
-      }
-    })
-  }
-
   render () {
-    let posts = <p style={{ textAlign: 'center' }}>Something want wrong !</p>
-    if (!this.state.error) {
-      posts = this.state.posts.map(post => {
-        return (
-          <Post
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-            key={post.id}
-          />
-        )
-      })
-    }
-
     return (
       <div className='Blog'>
         <header>
@@ -87,17 +15,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className='Posts'>{posts}</section>
-        <section>
-          <FullPost
-            id={this.state.selectedPostId}
-            loading={this.state.selectedPostLoading}
-            content={this.state.selectedPostContent}
-            deleted={this.postDeletePostHandler} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
+        <Posts />
       </div>
     )
   }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import classes from './Post.module.css'
+// import FullPost from '../../components/FullPost/FullPost'
+// import NewPost from '../../components/NewPost/NewPost'
+import Post from '../../../components/Post/Post'
+
+class Posts extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    selectedPostContent: null,
+    selectedPostLoading: false,
+    error: false
+  }
+
+  componentDidMount () {
+    const random = Math.random().toFixed(1)
+    const url = random >= 0.3 ? '/posts' : 'WrongUrl'
+    console.log(`if random < 0.3, get wrong url, random: ${random}, url: ${url}`)
+    axios
+      .get(url)
+      .then(res => {
+        const posts = res.data.slice(0, 4)
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Max'
+          }
+        })
+        this.setState({ posts: updatedPosts })
+      })
+      .catch(err => {
+        console.log(err)
+        this.setState({ error: true })
+      })
+  }
+
+  postSelectedHandler = id => {
+    if (this.state.selectedPostId !== id) {
+      this.setState({ selectedPostLoading: true })
+      axios.get(`/posts/${id}`).then(res => {
+        this.setState({
+          selectedPostId: id,
+          selectedPostContent: res.data,
+          selectedPostLoading: false
+        })
+      })
+    }
+  }
+
+  postDeletePostHandler = () => {
+    axios.delete(`/posts/${this.state.selectedPostId}`).then(res => {
+      if (res.status === 200) {
+        window.alert('Delete Post Success !')
+        this.setState({
+          selectedPostId: null,
+          selectedPostContent: null
+        })
+      }
+    })
+  }
+
+  render () {
+    let posts = <p style={{ textAlign: 'center' }}>Something want wrong !</p>
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return (
+          <Post
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+            key={post.id}
+          />
+        )
+      })
+    }
+    return (
+      <section className={classes.Post}>
+        {posts}
+      </section>
+    )
+  }
+}
+
+export default Posts
